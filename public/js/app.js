647793cd1ada90f7c4006d438e43c522a084d88b
Laurence <alaurenc@bmc.com>
2020-03-10 09:03:43 -0500

    Set up app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-const xxx = "abc"
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -12,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     const location = search.value; 
 
-    fetch('http://localhost:3000/weather?address=' + location).then( (response) => {
+    fetch('/weather?address=' + location).then( (response) => {
         response.json().then( (data) => {
             if (data.error) {
                 messageOne.textContent = data.error
